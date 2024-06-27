77699275bd8d3b5d2f900d4d4203d0e8a7b085db
Hrishi93 <sonumahure@gmail.com>
2024-06-27 20:47:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import { CartProvider } from './pages/CartContext';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <CartProvider>
+    <Router>
+      <App />
+    </Router>
+  </CartProvider>
+);
+
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
@@ -15,3 +35,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+*/
